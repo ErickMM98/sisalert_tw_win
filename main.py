0b9cd3fe99ad7e4130e48a09b76bddacabd81b0c
Erick Muñiz Morales <erickmuniz@ciencias.unam.mx>
2021-03-26 01:23:39 -0600

    Creamos la notificación

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ Programa que lee la página del sismológico nacional (SSN) para obtener una sen
 """
 
 import feedparser
+from win10toast import ToastNotifier
 
 
 url_ssn = "http://www.ssn.unam.mx/sismicidad/ultimos/"
 xml_ssn = "http://www.ssn.unam.mx/rss/ultimos-sismos.xml"
 
-lim_mag = 0
+lim_mag = 5.3
 
 def read_xml_last_sis():
     feed = feedparser.parse(xml_ssn)
@@ -25,10 +26,17 @@ def read_xml_last_sis():
 
     return title, mag, lat, lon
 
+def make_notificacion(title):
+    toaster = ToastNotifier()
+    toaster.show_toast("\t\t *** TENEMOS SISMO ***",
+                       title,
+                       duration=10)
 
-def show_not_windows(_mag):
+
+def show_not_windows(_mag,_title):
     if _mag > lim_mag:
-        continue
+        make_notificacion(_title)
 
 if __name__ == '__main__':
     title, mag, lat, lon = read_xml_last_sis()
+    show_not_windows(mag,title)
